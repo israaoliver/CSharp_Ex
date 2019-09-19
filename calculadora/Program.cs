0522a7abd46e777dc7f55a3f4fc18a7dde4ed3c7
israaoliver <isrraaoliveira@gmail.com>
2019-09-19 13:44:08 -0300

    first commit

---
+++ calculadora/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace calculadora
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 0;
+            String oper;
+
+           Console.WriteLine("Digite o 1º número:");
+           num1 = int.Parse(Console.ReadLine());
+           Console.WriteLine("Digite o 2º número:");
+           num2 = int.Parse(Console.ReadLine());
+           Console.WriteLine("Digite o operador:");
+           oper = Console.ReadLine();
+
+           Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+           Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+           Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+           Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
+           Console.WriteLine($"{num1} % {num2} = {num1 % num2}");
+
+
+        }
+    }
+}
---
+++ notas/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Notas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 0;
+            int num3 = 0;
+            int num4 = 0;
+
+            Console.WriteLine("Digite a nota do 1ºBimestre");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a nota do 2ºBimestre");
+            num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a nota do 3ºBimestre");
+            num3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a nota do 4ºBimestre");
+            num4 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($" 1ºBimestre {num1}");
+            Console.WriteLine($" 2ºBimestre {num2}");
+            Console.WriteLine($" 3ºBimestre {num3}");
+            Console.WriteLine($" 4ºBimestre {num4}");
+            Console.WriteLine("Media Geral");
+            Console.WriteLine($" = {(num1 + num2 + num3 + num4) / 4}");
+
+        }
+    }
+}
