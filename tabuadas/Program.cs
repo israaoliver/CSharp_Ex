a48a7019064b9dc1f56e448cac5bb0ce9fc517d7
israaoliver <isrraaoliveira@gmail.com>
2019-09-21 00:42:55 -0300

    casa

--- tabuadas/Program.cs
+++ tabuadas/Program.cs
@@ -6,20 +6,21 @@ namespace tabuado_5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("A tabuda do cinco é: ");
+            
+            Console.WriteLine("A tabuda do 1 ao 10  é: ");
 
-            for(int i=1; i <= 10; i++){
+            for(int i=1;i <= 10; i++){
 
-                Console.WriteLine($"{i} x 1=  " + (1 * i));
-                Console.WriteLine($"{i} x 2=  " + (2 * i));
-                Console.WriteLine($"{i} x 3 = " + (3 * i));
-                Console.WriteLine($"{i} x 4 = " + (4 * i));
-                Console.WriteLine($"{i} x 5 = " + (5 * i));
-                Console.WriteLine($"{i} x 6 = " + (6 * i));
-                Console.WriteLine($"{i} x 7 = " + (7 * i));
-                Console.WriteLine($"{i} x 8 = " + (8 * i));
-                Console.WriteLine($"{i} x 9 = " + (9 * i));
-                Console.WriteLine($"{i} x 10= " + (10 * i));
+                Console.WriteLine($"{i} x 1  = " + (1 * i));
+                Console.WriteLine($"{i} x 2  = " + (2 * i));
+                Console.WriteLine($"{i} x 3  = " + (3 * i));
+                Console.WriteLine($"{i} x 4  = " + (4 * i));
+                Console.WriteLine($"{i} x 5  = " + (5 * i));
+                Console.WriteLine($"{i} x 6  = " + (6 * i));
+                Console.WriteLine($"{i} x 7  = " + (7 * i));
+                Console.WriteLine($"{i} x 8  = " + (8 * i));
+                Console.WriteLine($"{i} x 9  = " + (9 * i));
+                Console.WriteLine($"{i} x 10 = " + (10 * i));
 
 
 
