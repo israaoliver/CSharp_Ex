ddb9edb350ec6095ba49dc919e64a36d54c40201
israaoliver <isrraaoliveira@gmail.com>
2019-10-02 17:17:34 -0300

    02/10

--- Exercio do RPG/KillTheDragon/Program.cs
+++ Exercio do RPG/KillTheDragon/Program.cs
@@ -25,17 +25,8 @@ namespace KillTheDragon
 
                     case "1":
                     Console.Clear();
-                    Warrior warrior = new Warrior(); 
-                    warrior.Name = "Haseid";
-                    warrior.LastName = "Faerûn";
-                    warrior.Hometown = "Brussel";
-                    warrior.BirthDate = DateTime.Parse("07/05/1371");
-                    warrior.AttackEquipment = "QuarterStaff";
-                    warrior.ProtectionEquipment ="LigthShield";                    
-                    warrior.Strong = 4;
-                    warrior.Agility = 3;
-                    warrior.Intelligence = 1;
-                    warrior.Life = 20;
+                    
+                    Warrior warrior = CharacterMaker();
 
                     Dragon dragon = new Dragon();
                     dragon.Name = "Smaug";
@@ -45,25 +36,20 @@ namespace KillTheDragon
                     dragon.Life = 300;
 
                     /* START - FIRST DIALOGUE */
-                    System.Console.WriteLine($"{warrior.Name.ToUpper()}:{dragon.Name}, you Crazy! been  killing him!");
-                    System.Console.WriteLine($"{dragon.Name.ToUpper()}: Human foolly.  who thinks it is? ");
 
-                    System.Console.WriteLine();
-                    System.Console.WriteLine("Press ENTER to continue");
-                    Console.ReadLine();
+                    DialogueMaker(warrior.Name, $"{dragon.Name}, you Crazy! been  killing him!");
+                    DialogueMaker(dragon.Name, "Human foolly.  who thinks it is?");
+
+                    DialogueEnd();
                     
                     /*END - FIRST DIALOGUE  */
 
                     /* START - SECOND DIALOGUE */
-                    System.Console.WriteLine($"{warrior.Name.ToUpper()}: I am {warrior.Name}! From the House {warrior.LastName}, oh evil creature!");
-                    System.Console.WriteLine($"{dragon.Name.ToUpper()}:WHO?? WHERE?? Good, whatever...  I will fry you and devour you, insolent ape!");
-
+                    DialogueMaker(warrior.Name, $"I am {warrior.Name}! From the House {warrior.LastName}, oh evil creature!");
+                    DialogueMaker(dragon.Name, $"WHO?? WHERE?? Good, whatever...  I will fry you and devour you, insolent ape!");
                     System.Console.WriteLine($"BAMBAM: Its show time!");
 
-                    System.Console.WriteLine();
-                    System.Console.WriteLine("Press ENTER to continue");
-                    Console.ReadLine();
-
+                    DialogueEnd();
                     /* END - SECOND DIALOGUE */
                     Console.Clear();
 
@@ -72,38 +58,39 @@ namespace KillTheDragon
                     int powerAttackWarrior = warrior.Strong > warrior.Intelligence ? warrior.Strong + warrior.Agility : warrior.Intelligence + warrior.Agility;
                     bool playerDontRun = true;
 
+                    Random generationRandomNumbers = new Random();
+                    int NumberRandomPlayer = generationRandomNumbers.Next(0, 5);
+                    int NumberRandomDragon = generationRandomNumbers.Next(0, 5);
+                    int warriorAgilityTotal = warrior.Agility + NumberRandomPlayer;
+                    int dragonAgilityTotal = dragon.Agility + NumberRandomDragon;
+                    
+
                     if(PlayerAttackFirts){
                         System.Console.WriteLine("*** Player Turn ***");
                         System.Console.WriteLine("Choose an Action");
                         System.Console.WriteLine(" 1 - Attack");
-                        System.Console.WriteLine(" 2 - escape");
+                        System.Console.WriteLine(" 2 - Escape");
 
                         string optionBattlePlayer = Console.ReadLine();
                         
                         switch (optionBattlePlayer)
                         {
                             case "1":
-                            Random generationRandomNumbers = new Random();
-                            int NumberRandomPlayer = generationRandomNumbers.Next(0, 5);
-                            int NumberRandomDragon = generationRandomNumbers.Next(0, 5);
-                            int warriorAgilityTotal = warrior.Agility + NumberRandomPlayer;
-                            int dragonAgilityTotal = dragon.Agility + NumberRandomDragon;
-
                             if (warriorAgilityTotal > dragonAgilityTotal) {
-                                System.Console.WriteLine($"{warrior.Name.ToUpper()}: Take it!! Lizard disgusting.");
+
+                                DialogueMaker(warrior.Name, "Take it!! Lizard disgusting.");
                                 dragon.Life -= powerAttackWarrior + 5;
                                 System.Console.WriteLine($"HP Dragon: {dragon.Life}");
                                 System.Console.WriteLine($"HP Warrior: {warrior.Life}");
                             } else{
-                                System.Console.WriteLine($"{dragon.Name.ToUpper()}: Missed , LittleBoy!! AHAHHAHAHA");
-                        
+                                DialogueMaker(warrior.Name, "Missed , LittleBoy!! AHAHHAHAHA"  );
                             }
                             
                             break;
 
                             case "2":
-                            System.Console.WriteLine($"{warrior.Name.ToUpper()}: Let's Go!");
-                            System.Console.WriteLine($"{dragon.Name.ToUpper()}: Run LittleBoy!! HAHAHAHAAHAH ");
+                            DialogueMaker(warrior.Name, "Let's Go!");
+                            DialogueMaker(dragon.Name, $"Run LittleBoy!! HAHAHAHAAHAH ");
                             playerDontRun = false;
                             break;
                             
@@ -111,77 +98,88 @@ namespace KillTheDragon
                         
                     }
 
-                    System.Console.WriteLine();
-                    System.Console.WriteLine("Press ENTER to Continue");
-                    Console.ReadLine();
+                    DialogueEnd();
 
                     while(warrior.Life > 0 && dragon.Life > 0 && playerDontRun)
                     {
+                        generationRandomNumbers = new Random();
+                        NumberRandomPlayer = generationRandomNumbers.Next(0, 5);
+                        NumberRandomDragon = generationRandomNumbers.Next(0, 5);
+                        warriorAgilityTotal = warrior.Agility + NumberRandomPlayer;
+                        dragonAgilityTotal = dragon.Agility + NumberRandomDragon;
                         Console.Clear();
                         System.Console.WriteLine("*** Dragon Turn ***");
-                        Random generationRandomNumbers = new Random();
-                            int NumberRandomPlayer = generationRandomNumbers.Next(0, 5);
-                            int NumberRandomDragon = generationRandomNumbers.Next(0, 5);
-                            int warriorAgilityTotal = warrior.Agility + NumberRandomPlayer;
-                            int dragonAgilityTotal = dragon.Agility + NumberRandomDragon;
-
                             if (dragonAgilityTotal > warriorAgilityTotal) {
-                                System.Console.WriteLine($"{dragon.Name.ToUpper()}: Burning you bastard!! ");
+                                DialogueMaker(dragon.Name, "Burning you bastard!!");
                                 warrior.Life = warrior.Life - dragon.Strong;
                                 System.Console.WriteLine($"HP Dragon: {dragon.Life}");
                                 System.Console.WriteLine($"HP Warrior: {warrior.Life}");
                             } else{
-                                System.Console.WriteLine($"{warrior.Name.ToUpper()}: is your fire going out!?");
+                                DialogueMaker(warrior.Name, "is your fire going out!?");
+                                System.Console.WriteLine($"HP Dragon: {dragon.Life}");
+                                System.Console.WriteLine($"HP Warrior: {warrior.Life}");
                         
                             } 
-                            
-                    System.Console.WriteLine();
-                    System.Console.WriteLine("Press ENTER to Continue");
-                    Console.ReadLine();   
 
-                    Console.Clear(); 
+                        DialogueEnd();
 
-                    
                         System.Console.WriteLine("*** Player Turn ***");
                         System.Console.WriteLine("Choose an Action");
                         System.Console.WriteLine(" 1 - Attack");
-                        System.Console.WriteLine(" 2 - escape");
+                        System.Console.WriteLine(" 2 - Escape");
 
                         string optionBattlePlayer = Console.ReadLine();
                         
                         switch (optionBattlePlayer)
                         {
                             case "1":
-                            generationRandomNumbers = new Random();
-                            NumberRandomPlayer = generationRandomNumbers.Next(0, 5);
-                            NumberRandomDragon = generationRandomNumbers.Next(0, 5);
-                            warriorAgilityTotal = warrior.Agility + NumberRandomPlayer;
-                            dragonAgilityTotal = dragon.Agility + NumberRandomDragon;
-
-                            if (warriorAgilityTotal > dragonAgilityTotal) {
-                                System.Console.WriteLine($"{warrior.Name.ToUpper()}: Take it!! Lizard disgusting.");
-                                dragon.Life -= powerAttackWarrior + 5;
-                                System.Console.WriteLine($"HP Dragon: {dragon.Life}");
-                                System.Console.WriteLine($"HP Warrior: {warrior.Life}");
-                            } else{
-                                System.Console.WriteLine($"{dragon.Name.ToUpper()}: Missed , LittleBoy!! AHAHHAHAHA");
-                        
-                            }
+                                if (warriorAgilityTotal > dragonAgilityTotal) {
+                                    DialogueMaker(warrior.Name, "Take it!! Lizard disgusting.");
+                                    dragon.Life -= powerAttackWarrior + 5;
+                                    System.Console.WriteLine($"HP Dragon: {dragon.Life}");
+                                    System.Console.WriteLine($"HP Warrior: {warrior.Life}");
+                                    Console.ReadLine();
+                                    
+                                } else{
+                                    DialogueMaker(dragon.Name, " Missed , LittleBoy!! AHAHHAHAHA");
+                                    System.Console.WriteLine($"HP Dragon: {dragon.Life}");
+                                    System.Console.WriteLine($"HP Warrior: {warrior.Life}");
+                                    Console.ReadLine();
+                                    
+                            
+                                }
                             
                             break;
 
                             case "2":
-                            System.Console.WriteLine($"{warrior.Name.ToUpper()}: Let's Go!");
-                            System.Console.WriteLine($"{dragon.Name.ToUpper()}: Run LittleBoy!! HAHAHAHAAHAH ");
+                            DialogueMaker(warrior.Name, "Let's Go!");
+                            DialogueMaker(dragon.Name, $"Run LittleBoy!! HAHAHAHAAHAH ");
                             playerDontRun = false;
                             break;
                             
                         }
+
+                        DialogueEnd();
                     
-                                       
+                    } 
+
+                    if(warrior.Life <= 0){
+                        System.Console.WriteLine("GAME OVER!");
+                        System.Console.WriteLine("Press ENTER to Continue");
+                        Console.ReadLine();
                     }
-                   
-                    break;                    
+                    if(dragon.Life <= 0){
+                        System.Console.WriteLine("YOU WIN!!");
+                        System.Console.WriteLine("CONGRATULATIONS");
+                        System.Console.WriteLine("Press ENTER to Continue");
+                        Console.ReadLine();
+                    }
+                    break;  
+
+                    /**
+                     * todo: Exemplo de alerta pendente
+                     * ! Exemplo de alerta urgente
+                     */                  
 
                     case "0":
                     PlayerdontGiveUp = false;
@@ -197,5 +195,40 @@ namespace KillTheDragon
 
             
         }
+
+    public static void DialogueMaker(String name, string phrase)
+        {
+        
+            System.Console.WriteLine($"{name.ToUpper()}:{phrase}");
+
+        }
+    
+    public static void DialogueEnd()
+    {
+
+        System.Console.WriteLine();
+        System.Console.WriteLine("Press ENTER to continue");
+        Console.ReadLine();
+        Console.Clear();
+
+    }
+
+    public static Warrior CharacterMaker()
+    {
+        Warrior warrior = new Warrior(); 
+        warrior.Name = "Haseid";
+        warrior.LastName = "Faerûn";
+        warrior.Hometown = "Brussel";
+        warrior.BirthDate = DateTime.Parse("07/05/1371");
+        warrior.AttackEquipment = "QuarterStaff";
+        warrior.ProtectionEquipment ="LigthShield";                    
+        warrior.Strong = 4;
+        warrior.Agility = 3;
+        warrior.Intelligence = 1;
+        warrior.Life = 20;
+
+        return warrior;
+    }
+
     }
 }
